14f3c1d9b152969a04b5d2915163cb99460b351d
Win <synwe@yahoo.com>
2017-03-27 12:25:30 +0000

    Correction

--- vvpages/views.py
+++ vvpages/views.py
@@ -1,20 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import json
-from django.views.generic.base import View
-from django.http import JsonResponse
-from django.conf import settings
-from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
-from instant.producers import publish
+from django.http import Http404
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from vvpages.serializers import PageSerializer
+from vvpages.models import Page
 
 
-class PostMsgView(LoginRequiredMixin, SuperuserRequiredMixin, View):
-    login_url = settings.LOGIN_URL
+class PageRestView(APIView):
     
-    def post(self, request, *args, **kwargs):
-        data = json.loads(self.request.body.decode('utf-8'))
-        msg = data["msg"]
-        channel = data["channel"]
-        event_class = data["event_class"]
-        publish(message=msg, event_class=event_class, channel=channel)
-        return JsonResponse({"ok":1})
+    def get_object(self, id):
+        try:
+            return get_object_or_404(Page, pk=self.kwargs['pk'], published=True)
+        except Page.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, *args, **kwargs):
+        page = self.get_object(id)
+        serializer = PageSerializer(page)
+        return Response(serializer.data)
+    
+    def get_queryset(self, *args, **kwargs):
+        q = Page.objects.filter(pk=self.kwargs['pk'], published=True)
+        return q
+
+
+class SitemapRestView(TemplateView):
+    template_name = "vvpages/sitemap.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(SitemapRestView, self).get_context_data(**kwargs)
+        root_node, created = Page.objects.get_or_create(url="/")
+        if created is True:
+            root_node.title = "Home"
+            root_node.save()
+        nodes = root_node.get_descendants(include_self=True).filter(published=True)
+        context['nodes'] = nodes
+        return context
